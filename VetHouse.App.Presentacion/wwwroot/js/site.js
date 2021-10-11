e7879c88b489759080666ec8e4e773602e5c7c46
Luis Felipe Ceballos Caicedo <ceballos-65@hotmail.com>
2021-10-11 14:51:28 -0500

    Add functionality to register form }

--- VetHouse.App.Presentacion/wwwroot/js/site.js
+++ VetHouse.App.Presentacion/wwwroot/js/site.js
@@ -11,3 +11,41 @@
 //         this.classList.add("invalid");
 //     }
 // }
+
+let currentFormRegister = 0;
+const formCurrent = document.querySelectorAll(".form-register");
+const btnActive = document.querySelectorAll(".btn");
+
+
+showForm(currentFormRegister, formCurrent);
+
+function showForm(n, formCurrent) {
+    formCurrent[n].style.display = "block";
+
+    if (n === 0) {
+        document.querySelector(".btn-back").style.display = "none";
+    } else {
+        document.querySelector(".btn-back").style.display = "inline";
+    }
+
+    if (n == (formCurrent.length - 1)) {
+        document.querySelector(".btn-next").innerHTML = "SUBMIT";
+    } else {
+        document.querySelector(".btn-next").innerHTML = "NEXT";
+    }
+}
+
+
+btnActive.forEach(btn => {
+    btn.addEventListener('click', () => {
+        formCurrent[currentFormRegister].style.display = "none";
+        
+        if (btn.textContent.toUpperCase() === "NEXT") {
+            currentFormRegister += 1;
+        } else if (btn.textContent.toUpperCase() === "BACK") {
+            currentFormRegister -= 1;
+        }
+
+        showForm(currentFormRegister, formCurrent);
+    });
+});
